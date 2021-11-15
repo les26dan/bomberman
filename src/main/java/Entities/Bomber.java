57d7ef871b4c81a6c16c513b93e29f7bdc0c9bce
Truong Minh Tri <minhtrithcslqd1@gmail.com>
2021-11-15 21:33:13 +0700

    Add again.

---
+++ src/main/java/Entities/Bomber.java
@@ -0,0 +1,49 @@
+package Entities;
+
+public class Bomber extends MobileEntity {
+    protected int hearts;
+    protected int numBomb;
+    protected int score;
+    protected int sizeBomb;
+
+
+    public Bomber(int x, int y, int speed, int direction, int hearts, int numBomb, int score, int sizeBomb) {
+        super(x, y, speed, direction);
+        this.hearts = hearts;
+        this.numBomb = numBomb;
+        this.score = score;
+        this.sizeBomb = sizeBomb;
+    }
+
+    public int getHearts() {
+        return hearts;
+    }
+
+    public void setHearts(int hearts) {
+        this.hearts = hearts;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getNumBomb() {
+        return numBomb;
+    }
+
+    public void setNumBomb(int numBomb) {
+        this.numBomb = numBomb;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
---
+++ src/main/java/Entities/Enemy/Oneal.java
@@ -0,0 +1,10 @@
+package Entities.Enemy;
+
+public class Oneal extends Enemy {
+
+    public Oneal(int x, int y, int speed, int direction, int value) {
+        super(x, y, speed, direction, value);
+    }
+
+
+}
