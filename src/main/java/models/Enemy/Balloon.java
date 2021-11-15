c9abe832aa4c0296be8a8522923b732c1a618785
Truong Minh Tri <minhtrithcslqd1@gmail.com>
2021-11-15 21:26:58 +0700

    Change source code structure

--- src/main/java/models/Enemy/Balloon.java
+++
@@ -1,8 +0,0 @@
-package models.Enemy;
-
-public class Balloon extends Enemy {
-
-    public Balloon(int x, int y, int speed, int direction, int value) {
-        super(x, y, speed, direction, value);
-    }
-}
--- src/main/java/models/Enemy/Oneal.java
+++
@@ -1,8 +0,0 @@
-package models.Enemy;
-
-public class Oneal extends Enemy {
-
-    public Oneal(int x, int y, int speed, int direction, int value) {
-        super(x, y, speed, direction, value);
-    }
-}
