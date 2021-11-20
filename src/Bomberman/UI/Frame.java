45ca15755603cce9f76c0cecb01a44464818f48b
Le Sy Dan <les26dan@gmail.com>
2021-11-20 17:34:53 +0700

    - Add UI:  + Frame.java  + GamePanel.java - Add Keyboard/Keyboard.java

---
+++ src/Bomberman/UI/Frame.java
@@ -0,0 +1,29 @@
+package Bomberman.UI;
+
+import Bomberman.Game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+
+  public GamePanel gamePanel;
+  private JPanel headPanel;
+  private Game game;
+
+  public Frame() {
+    headPanel = new JPanel(new BorderLayout());
+    gamePanel = new GamePanel();
+    headPanel.add(gamePanel, BorderLayout.PAGE_END);
+    add(headPanel);
+    game = gamePanel.getGame();
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setTitle("Bomberman");
+    setResizable(false);
+    pack();
+    setVisible(true);
+    setLocationRelativeTo(null);
+    game.start();
+  }
+
+}
---
+++ src/Bomberman/UI/GamePanel.java
@@ -0,0 +1,28 @@
+package Bomberman.UI;
+
+import Bomberman.Game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+  private Game game;
+
+  public GamePanel() {
+    setLayout(new BorderLayout());
+    setPreferredSize(new Dimension(Game.WIDTH * Game.SCALE, Game.HEIGHT * Game.SCALE));
+    game = new Game();
+    add(game);
+
+    game.setVisible(true);
+    setVisible(true);
+    setFocusable(true);
+
+  }
+
+  public Game getGame() {
+    return game;
+  }
+
+}
