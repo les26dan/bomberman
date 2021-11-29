d35e4f7c8fc8f91fa2d92bf6fcd8b56d1d4d5c86
Truong Minh Tri <minhtrithcslqd1@gmail.com>
2021-11-29 16:28:45 +0700

    Create Ballon to GameContainer.

--- src/Bomberman/level/Level.java
+++ src/Bomberman/level/Level.java
@@ -2,7 +2,11 @@ package Bomberman.level;
 
 import Bomberman.Entities.Box.Grass;
 import Bomberman.Entities.Box.Wall;
+import Bomberman.Entities.Dynamic.Bomber;
+import Bomberman.Entities.Dynamic.Enemy.Balloon;
+import Bomberman.Game;
 import Bomberman.GameContainer;
+import Bomberman.graphics.Coordinate;
 import Bomberman.graphics.Sprite;
 
 import java.io.BufferedReader;
@@ -16,12 +20,12 @@ public class Level {
     protected String[] map;
     protected GameContainer gameContainer;
 
-    public Level(String path, GameContainer gameContainer) throws IOException{
+    public Level(String path, GameContainer gameContainer) throws IOException {
         load(path);
         this.gameContainer = gameContainer;
     }
 
-    public void load(String path) throws IOException{
+    public void load(String path) throws IOException {
 
         URL url = Level.class.getResource("/" + path);
 
@@ -41,7 +45,7 @@ public class Level {
             map[i] = inp.readLine().substring(0, width);
         }
 
-       inp.close();
+        inp.close();
     }
 
     public void createEntities() {
@@ -50,12 +54,18 @@ public class Level {
                 char c = map[y].charAt(x);
                 int pos = x + y * width;
 
-                switch(c) {
+                switch (c) {
                     case '#':
                         gameContainer.addEntitie(pos, new Wall(x, y));
                         break;
+                    case '1':
+                        gameContainer.addEntitie(pos, new Grass(x, y));
+                        gameContainer.addDynamicEntity(new Balloon(Coordinate.posToPixel(x), Coordinate.posToPixel(y) + Game.BOX_SIZE, gameContainer));
+                        break;
+                    case 'p':
+                        gameContainer.addDynamicEntity(new Bomber(Coordinate.posToPixel(x), Coordinate.posToPixel(y) + Game.BOX_SIZE, gameContainer));
                     default:
-                        gameContainer.addEntitie(pos, new Grass(x, y) );
+                        gameContainer.addEntitie(pos, new Grass(x, y));
                         break;
                 }
             }
