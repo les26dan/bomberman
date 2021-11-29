c2984e7a23e479bce9b5e4e771b6672e674b1208
Truong Minh Tri <minhtrithcslqd1@gmail.com>
2021-11-29 16:33:15 +0700

    Add method to Enemy: Move, canMove, render and loadSprite.

--- src/Bomberman/Entities/Dynamic/Enemy/Enemy.java
+++ src/Bomberman/Entities/Dynamic/Enemy/Enemy.java
@@ -1,20 +1,85 @@
 package Bomberman.Entities.Dynamic.Enemy;
 
 import Bomberman.Entities.Dynamic.DynamicEntity;
+import Bomberman.Entities.Entity;
+import Bomberman.Game;
+import Bomberman.graphics.Coordinate;
+import Bomberman.graphics.Screen;
 
 public abstract class Enemy extends DynamicEntity {
     protected int value;
+    protected double steps;
 
-    public Enemy(int x, int y) {
+    public Enemy(double x, double y) {
         super(x, y);
+        this.speed = 0.5;
+        this.steps = Game.BOX_SIZE * 1.0 / speed;
     }
 
-    public Enemy(int x, int y, int speed, int direction, int value) {
+    public Enemy(double x, double y, int speed, int direction, int value) {
         super(x, y, speed, direction);
         this.value = value;
     }
 
-    public int getValue() {
-        return value;
+    protected abstract void loadSprite();
+
+    @Override
+    public void update() {
+        move();
+    }
+
+    @Override
+    public void render(Screen screen) {
+        loadSprite();
+        screen.renderEntity((int) x, (int) y - sprite.SIZE, this);
+    }
+
+    @Override
+    protected void move() {
+        double u = x;
+        double v = y;
+        if (steps <= 0) {
+            direction = (int) (Math.random() * 100 + 1) % 4;
+            steps = Game.BOX_SIZE * 1.0 / speed;
+        }
+        if (direction == 0)
+            v -= 1 * speed;
+        if (direction == 2)
+            v += 1 * speed;
+        if (direction == 3)
+            u -= 1 * speed;
+        if (direction == 1)
+            u += 1 * speed;
+        moving = u != x || v != y;
+        if (canMove( (u - x) * 2, (v - y) * 2)) {
+            steps -= 1;
+            x = u;
+            y = v;
+        } else {
+            steps = 0;
+        }
+    }
+
+    @Override
+    protected boolean canMove(double addX, double addY) {
+        double xx = x, yy = y - 16;
+        if (direction == 0) {
+            yy += 15;
+            xx += 8;
+        }
+        if (direction == 1) {
+            yy += 8;
+            xx += 1;
+        }
+        if (direction == 2) {
+            xx += 8;
+            yy += 1;
+        }
+        if (direction == 3) {
+            xx += 15;
+            yy += 8;
+        }
+        Entity e = gameContainer.getEntity(Coordinate.pixelToPos(xx) + (int) addX, Coordinate.pixelToPos(yy) + (int) addY, this);
+        return !e.getBlock();
     }
 }
