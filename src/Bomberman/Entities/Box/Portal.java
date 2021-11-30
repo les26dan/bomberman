0a6345bfd25480f02e7e0319c6a0f2192495359f
Truong Minh Tri <minhtrithcslqd1@gmail.com>
2021-11-30 23:20:35 +0700

    Add Portal.

---
+++ src/Bomberman/Entities/Box/Portal.java
@@ -0,0 +1,10 @@
+package Bomberman.Entities.Box;
+
+import Bomberman.graphics.Sprite;
+
+public class Portal extends Box {
+    public Portal(int x, int y) {
+        super(x, y);
+        sprite = Sprite.portal;
+    }
+}
