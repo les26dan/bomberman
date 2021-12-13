1db2f4900a0e04eec6357272968885e8616bcb9c
Truong Minh Tri <minhtrithcslqd1@gmail.com>
2021-12-13 10:06:31 +0700

    Add Ovape(Enemy).

--- src/Bomberman/Entities/Dynamic/Enemy/Ovape.java
+++ src/Bomberman/Entities/Dynamic/Enemy/Ovape.java
@@ -1,4 +1,20 @@
 package Bomberman.Entities.Dynamic.Enemy;
 
-public class Ovape {
+import Bomberman.Game;
+import Bomberman.GameContainer;
+import Bomberman.graphics.Sprite;
+
+public class Ovape extends Enemy {
+
+    public Ovape(double x, double y, GameContainer gameContainer) {
+        super(x, y, Sprite.ovape_dead);
+        this.gameContainer = gameContainer;
+        this.speed = 0.5;
+        this.steps = Game.BOX_SIZE * 1.0 / speed;
+    }
+
+    @Override
+    protected void loadSprite() {
+
+    }
 }
